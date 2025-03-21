2c530d04baa2e294a051c4c1a7339f2066035a9a
balyaGround <balyazulfikar@gmail.com>
2025-03-21 22:37:11 +0700

    dah rapi sliderlist koz

--- amikos/src/components/RentalList.js
+++ amikos/src/components/RentalList.js
@@ -1,11 +1,10 @@
-// RentalList.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "../App.css";
+import '../styles/Rentallist.css';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import { FaBroom, FaShieldAlt, FaMapMarkerAlt, FaMoneyBillWave, FaStar } from "react-icons/fa";
+import { FaHome, FaShieldAlt, FaMapMarkerAlt, FaMoneyBillWave, FaStar } from "react-icons/fa";
 
 const RentalList = ({ rentals }) => {
   if (rentals.length === 0) {
@@ -16,49 +15,41 @@ const RentalList = ({ rentals }) => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 2, // Number of cards visible at once
+    slidesToShow: 3, // Shows 3 cards at once
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 2 }
-      },
-      {
-        breakpoint: 768,
-        settings: { slidesToShow: 1 }
-      }
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } }
     ]
   };
 
   return (
-    <div className="container rental-container">
-      <h2 className="menu-title text-success">Temukan Kos Terbaikmu !</h2>
+    <div className="rental-fullwidth">
+      <h2 className="rental-title">Temukan Kos Terbaikmu!</h2>
       <Slider {...settings} className="rental-slider">
         {rentals.map((rental) => (
-          <div key={rental.id} className="menu-item">
-            <div className="menu-card">
-              {/* Image */}
+          <div key={rental.id} className="rental-item">
+            <div className="rental-card">
+              {/* Left Side - Image */}
               {rental.foto && (
-                <div className="menu-image">
+                <div className="rental-image">
                   <img src={rental.foto} alt={rental.nama_tempat} />
                 </div>
               )}
-              
-              {/* Details */}
-              <div className="menu-details">
-                <h3 className="menu-card-title">{rental.nama_tempat || "Nama Tidak Tersedia"}</h3>
-               <p className="menu-description">
-                  <span><FaBroom className="icon" /> Kebersihan:</span> {rental.kebersihan || 0} <br />
-                  <span><FaShieldAlt className="icon" /> Keamanan:</span> {rental.keamanan || 0} <br />
-                  <span><FaMapMarkerAlt className="icon" /> Jarak:</span> {rental.jarak_dari_kampus || 0} km <br />
-                  <span><FaMoneyBillWave className="icon" /> Harga:</span> Rp {rental.harga_sewa ? rental.harga_sewa.toLocaleString() : 'Tidak Tersedia'} <br />
-                  <span><FaStar className="icon" /> Fasilitas:</span> {rental.fasilitas || 0}
+
+              {/* Right Side - Description */}
+              <div className="rental-details">
+                <h3 className="rental-name">{rental.nama_tempat || "Nama Tidak Tersedia"}</h3>
+                <p className="rental-info">
+                  <FaStar className="rental-icon" /> <span>Kebersihan:</span> {rental.kebersihan || 0} <br />
+                  <FaShieldAlt className="rental-icon" /> <span>Keamanan:</span> {rental.keamanan || 0} <br />
+                  <FaMapMarkerAlt className="rental-icon" /> <span>Jarak:</span> {rental.jarak_dari_kampus || 0} km <br />
+                  <FaHome className="rental-icon" /> <span>Fasilitas:</span> {rental.fasilitas || 0} <br />
+                  <FaMoneyBillWave className="rental-icon" /> <span>Harga:</span> Rp {rental.harga_sewa ? rental.harga_sewa.toLocaleString() : 'Tidak Tersedia'} 
                 </p>
-                <Link to={`/purchase/${rental.id}`} className="rent-button">
-                  Rent Now
-                </Link>
+                <Link to={`/purchase/${rental.id}`} className="rent-btn">Rent Now</Link>
               </div>
             </div>
           </div>
@@ -67,7 +58,5 @@ const RentalList = ({ rentals }) => {
     </div>
   );
 };
-
-
 
 export default RentalList;
